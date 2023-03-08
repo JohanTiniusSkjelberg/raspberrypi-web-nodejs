364f262a87ebc69f7477ec8ca3e62cf009bc044f
Johan Tinius <johantinius@hotmail.com>
2023-03-08 18:35:32 +0100

    Adding react

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+
+function App() {
+  const [mealData,setMealData] = useState(null);
+  const [calories,setCalories] = useState(2000);
+
+  return <div className="App"></div>;
+}
+
+export default App;
